a2b9c9bf54e26f59b874ac95afc8fffad14ffad9
MinyoungChoi <youngdev57@gmail.com>
2020-07-09 23:55:04 +0900

    C1 - Java file push test

--- wefal/app/src/main/java/com/wefal/MainActivity.java
+++ wefal/app/src/main/java/com/wefal/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    public final int VERSION = 0x01;
+    public final int VERSION = 0x02;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
