10bf00e00116cb24ca433596141eb62c9e71bf06
rpdst <cjfdls50@gmail.com>
2020-07-09 23:52:40 +0900

    K2 - Java 파일 푸시 테스트

--- wefal/app/src/main/java/com/wefal/MainActivity.java
+++ wefal/app/src/main/java/com/wefal/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    public final int VERSION = 0x01;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
