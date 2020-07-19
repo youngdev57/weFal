20b3ed7f557d7eae22504b0c8ff37fff0ab546bd
rpdst <cjfdls20000@naver.com>
2020-07-19 15:07:32 +0900

    K4 - JoinActivity (Insert Test OK)

--- wefal/app/src/main/java/com/wefal/JoinActivity.java
+++ wefal/app/src/main/java/com/wefal/JoinActivity.java
@@ -5,27 +5,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class JoinActivity extends AppCompatActivity {
     Button join_btn = null;
-    EditText join_id = null, join_pwd = null;
+    EditText join_email = null, join_pwd = null;
 
-    String id = "", pwd = "";
+    String email = "", pwd = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,86 +28,34 @@ public class JoinActivity extends AppCompatActivity {
         setContentView(R.layout.activity_join);
 
         join_btn = findViewById(R.id.join_btn);
-        join_id = findViewById(R.id.join_id_editText);
+        join_email = findViewById(R.id.join_id_editText);
         join_pwd = findViewById(R.id.join_pwd_editText);
 
         join_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                id = join_id.getText().toString();
+                email = join_email.getText().toString();
                 pwd = join_pwd.getText().toString();
 
-                System.out.println(id + "**********************");
+                System.out.println(email + "**********************");
                 System.out.println(pwd + "**********************");
-                //HttpPostData();
                 InsertData task = new InsertData();
-                task.execute("http://rpdst.dothome.co.kr/php/insert.php", id, pwd);
+                task.execute("http://missing.dothome.co.kr/wefal/insertUser.php", email, pwd);
                 Toast.makeText(getApplicationContext(), "전송 후 결과 받음", Toast.LENGTH_SHORT).show();
             }
         });
     }
-
-    public void HttpPostData() {
-        try {
-            //--------------------------
-            //   URL 설정하고 접속하기
-            //--------------------------
-            String postData = "id="+id+"&pwd="+pwd;
-            URL url = new URL("http://missing.dothome.co.kr/wefal/user/insertUser.php");       // URL 설정
-            HttpURLConnection http = (HttpURLConnection) url.openConnection();   // 접속
-            System.out.println("코드: "+http.getResponseCode());
-            //--------------------------
-            //   전송 모드 설정 - 기본적인 설정이다
-            //--------------------------
-            http.setDefaultUseCaches(false);
-            http.setDoInput(true);                        // 서버에서 읽기 모드 지정
-            http.setDoOutput(true);                       // 서버로 쓰기 모드 지정
-            http.setRequestMethod("GET");
-            http.connect();
-
-            // 서버에게 웹에서 <Form>으로 값이 넘어온 것과 같은 방식으로 처리하라는 걸 알려준다
-            http.setRequestProperty("content-type", "application/x-www-form-urlencoded");
-            //--------------------------
-            //   서버로 값 전송
-            //--------------------------
-            StringBuffer buffer = new StringBuffer();
-//            buffer.append("id").append("=").append(id).append("&");                 // php 변수에 값 대입
-//            buffer.append("pwd").append("=").append(pwd);   // php 변수 앞에 '$' 붙이지 않는다
-
-            OutputStream outputStream = http.getOutputStream();
-            outputStream.write(postData.getBytes("UTF-8"));
-            outputStream.flush();
-            outputStream.close();
-            //--------------------------
-            //   서버에서 전송받기
-            //--------------------------
-            InputStreamReader tmp = new InputStreamReader(http.getInputStream(), "EUC-KR");
-            BufferedReader reader = new BufferedReader(tmp);
-            StringBuilder builder = new StringBuilder();
-            String str;
-            while ((str = reader.readLine()) != null) {       // 서버에서 라인단위로 보내줄 것이므로 라인단위로 읽는다
-                builder.append(str + "\n");                     // View에 표시하기 위해 라인 구분자 추가
-            }
-            String result = builder.toString();
-            ((TextView)(findViewById(R.id.join_pwdCheck_editText))).setText(result);
-
-        } catch (MalformedURLException e) {
-            //
-        } catch (IOException e) {
-            //
-        } // try
-    } // HttpPostData
 
     class InsertData extends AsyncTask<String, Void, String>{
 
         @Override
         protected String doInBackground(String... params) {
 
-            String id = (String)params[1];
+            String email = (String)params[1];
             String pwd = (String)params[2];
             String serverURL = (String)params[0];
 
-            String postParameters = "id=" + id + "&pwd=" + pwd;
+            String postParameters = "email=" + email + "&pwd=" + pwd;
 
             try {
                 System.out.println("try 직후 실행");
