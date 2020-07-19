22652e8100bff86e00d841db7ce1a91a39b5ccbd
rpdst <cjfdls20000@naver.com>
2020-07-19 17:18:58 +0900

    K7 - 회원가입 유효성 검사 추가

--- wefal/app/src/main/java/com/wefal/LoginActivity.java
+++ wefal/app/src/main/java/com/wefal/LoginActivity.java
@@ -5,12 +5,14 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
-    Button master_main_btn = null;
+    Button master_main_btn = null, login_btn = null;
+    EditText login_email = null, login_pwd = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,6 +26,17 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        login_email = findViewById(R.id.login_email_editText);
+        login_pwd = findViewById(R.id.login_pwd_editText);
+
+        login_btn = findViewById(R.id.login_btn);
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //로그인 작업..
             }
         });
     }
