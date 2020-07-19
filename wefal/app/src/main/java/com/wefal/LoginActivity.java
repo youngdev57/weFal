d7269470399bdc8e5b162e63537f416f27a53cc4
rpdst <cjfdls20000@naver.com>
2020-07-19 15:35:32 +0900

    K5 - LoginActivity, activity_login 생성

---
+++ wefal/app/src/main/java/com/wefal/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.wefal;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
